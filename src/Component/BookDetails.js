f3585a2e0d6fdc2c15520810a8c9b942bd89ef70
duc-minh-droid <ducminhcsp@gmail.com>
2022-01-16 16:48:09 +0700

    deploy

--- src/Component/BookDetails.js
+++ src/Component/BookDetails.js
@@ -5,9 +5,11 @@ function BookDetails({book}) {
     const { dispatch } = useContext(BookContext)
 
     return (
-        <li className='flexli' onClick={()=>dispatch({ type: "REMOVE_BOOK", id: book.id})}>
-            <div className='title'>{book.title}</div>
-            <div className='author'>{book.author}</div>
+        <li onClick={()=>dispatch({ type: "REMOVE_BOOK", id: book.id})}>
+            <div className='flexli'>
+                <div className='title'>{book.title}</div>
+                <div className='author'>{book.author}</div>
+            </div>
         </li>
     )
 }
